6f6c8ad475cdfd97aac99efcc8324b19f45aa069
Wolf <d.wolftripp@gmail.com>
2023-05-05 13:18:43 -0700

    deploy

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -19,7 +19,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 # set up connection to db
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/wolf_collector_mk2"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
